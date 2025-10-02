112490ea2cc3506c9cf4516871524c93d9565188
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-10-02 20:55:39 -0300

    subo configuracion

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,13 @@ const app = express()
 app.use(cors({
   origin: 'http://localhost:5173' // Puerto que utilizo en el frontend
 })); 
+
 //utilizo libreria
 app.use(express.json())
 
 const prueba = require('./controllers/prueba')
 
 app.use('/prueba', prueba)
-
-// app.use('/', (req, res)=>{
-//    return res.status(200).json('Peticion de prueba jaja jeje jiji jojo juju')
-// })
 
 app.listen(8000, () => {
     console.log("Servidor corriendo en el puerto 8000")
