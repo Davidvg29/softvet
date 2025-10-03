08462a084896b8ff57ae556f6c18b2186d7d907f
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-10-02 21:11:47 -0300

    configuro variables de entorno

--- backend/config/bd/dataBase.js
+++ backend/config/bd/dataBase.js
@@ -1,10 +1,13 @@
+require('dotenv').config(); // asegurate de cargar dotenv
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'db_softvet'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
