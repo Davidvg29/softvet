b50e33a0e565500d8471dc8017eb220deaf4436d
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-10-23 18:34:17 -0300

    agrego controlador de stock y ruteo del mismo

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,11 @@ app.use(morgan('dev'));
 
 const prueba = require('./routers/prueba');
 const mascotasRouter  = require("./routers/mascotas");
+const stockRouter = require("./routers/stock");
 
 app.use('/', prueba)
 app.use('/mascotas', mascotasRouter);
+app.use('/stock', stockRouter);
 
 app.listen(8000, () => {
     console.log("Servidor corriendo en el puerto 8000")
