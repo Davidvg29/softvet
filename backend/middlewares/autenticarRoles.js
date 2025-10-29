d5630ddc8b1b639bd3a219f5ca20502ae3138a34
Davidvg29 <davidpivito@gmail.com>
2025-10-29 09:23:21 -0300

    quito log

--- backend/middlewares/autenticarRoles.js
+++ backend/middlewares/autenticarRoles.js
@@ -1,15 +1,10 @@
 const autenticarRoles = (allowedRoles) => {
     return (req, res, next) => {
         try {
-          console.log(req);
-          
             if (!allowedRoles.includes(req.empleado_softvet.nombre_rol)) {
                 return res.status(403).json({ error: 'Acceso prohibido. Rol no autorizado.' });
             }
-
-            // Si todo es correcto, continuar con el siguiente middleware/controlador
             next();
-
         } catch (err) {
             res.status(401).json({
                 status: false,
