bf2474bb8131b81a8fe696029acf213935ced3e8
joaquindiaz21 <joaquindiaz2103@gmail.com>
2025-10-29 21:37:29 -0300

    Agrego componente Formulario a la landing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../components/landing/Header';
 import SoftvetInfo from '../components/landing/SoftvetInfo';
 import SoftVetGestion from '../components/landing/SoftVetGestion';
 import SoftVetDemo from '../components/landing/SoftVetDemo';
+import SoftVetForm from '../components/landing/SoftVetForm';
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
        <Carrousel/>
        <section id="gestion" className="py-5">
        <SoftVetGestion/>
+       <SoftVetForm/>
       </section>
     </>
   );
