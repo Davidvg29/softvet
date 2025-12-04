f4b48419903fe7a26ca77df52ab5e8c050226f47
Davidvg29 <davidpivito@gmail.com>
2025-12-04 15:17:53 -0300

    configuro para deploy

--- backend/config/bd/dataBase.js
+++ backend/config/bd/dataBase.js
@@ -7,7 +7,10 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true,
+    },
 });
 
 connection.connect((err) => {
